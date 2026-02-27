4569de7cc16b3275ab8f99ddc1e9f03b06772d16
Manuel Ortega <manuel.ortega@synacy.com>
2026-02-28 00:24:50 +0800

    Increase JSON body limit to 10mb for image uploads

--- server/src/index.js
+++ server/src/index.js
@@ -28,8 +28,8 @@ initializeSocket(server);
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Request logging middleware
 app.use((req, res, next) => {
